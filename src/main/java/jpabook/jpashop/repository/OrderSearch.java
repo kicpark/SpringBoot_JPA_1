36996b22c622c6f11e42ff75285151eff6cd1324
unknown <pkch0102@gmail.com>
2024-10-16 18:52:10 +0900

    주문 도메인 개발

---
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+    private String memberName;
+    private OrderStatus orderStatus;
+}
